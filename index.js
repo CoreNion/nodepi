e67239c2c4c09ce1215eb7ee532cef501110bd19
CoreNion <coreinion@gmail.com>
2023-10-31 15:44:30 +0900

    Arudino/I2C経由で温度計の温度を取得し表示

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const http = require("http");
 const gpio = require('rpi-gpio');
+var i2c = require('i2c-bus');
 
 const port = 3000;
+const DEVICE_NUMBER = 1;
+const TARGET_IC_ADDR = 0x08;
 
 gpio.setup(8, gpio.DIR_OUT);
 
@@ -17,12 +20,21 @@ const server = http.createServer((request, response) => {
     gpio.write(8, false);
 
     console.log("GPIO Off~")
+  } else if (request.url === "/temp") {
+    var i2c1 = i2c.openSync(DEVICE_NUMBER);
+
+    var readBuf = new Buffer.alloc(0x2);
+    i2c1.i2cReadSync(TARGET_IC_ADDR, readBuf.length, readBuf);
+
+    // 16進数から数値に変換
+    mesg = `現在の温度は${readBuf.readInt8(0)}度です`;
+    console.log(readBuf);
   } else {
     mesg = "no command";
   }
 
   response.writeHead(200, {
-    "Content-Type": "text/html"
+    "Content-Type": "text/html; charset=utf-8"
   });
 
 
